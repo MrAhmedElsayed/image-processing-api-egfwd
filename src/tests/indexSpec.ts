d91c00d6b91dce114b86e0e8ad5cf6e4b64a7a48
MrAhmedElsayed <ahmedsayed551991@gmail.com>
2022-03-26 07:42:52 +0300

    fix test failure

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -15,19 +15,16 @@ it('should return the directory path', () => {
 // Create a test in an asynchronous way to change the image size by entering the file name,
 // width, height and check whether the image has been converted correctly
 describe('resizeImage (for front-end functionality testing)', () => {
-  it('Check if image found after resize (front-end)', async () => {
+  it('Check if sent as buffer as response (front-end)', async () => {
+    const default_thumbnail_directory = './public/thumbnails/'
+    await createOrReturnDirectory(default_thumbnail_directory)
     const base64Image = await imageToBase64(
       './public/demo-images/icelandwaterfall.jpg'
     )
-    const imageBuffer = base64ToBuffer(base64Image)
-    const image = await resizeImage(
-      imageBuffer,
-      100,
-      100,
-      'icelandwaterfall',
-      'jpg'
-    )
-    expect(image).toBeDefined()
+    expect(async () => {
+      const imageBuffer = base64ToBuffer(base64Image)
+      await resizeImage(imageBuffer, 100, 100, 'icelandwaterfall', 'jpg')
+    }).not.toThrow()
   })
 })
 
